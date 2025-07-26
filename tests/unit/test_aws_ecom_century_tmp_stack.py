06230939177cdd82a67f4b996b4efc69f45d7c8b
Gopala krishna Animisetti <44962156+gopala5564@users.noreply.github.com>
2025-07-26 12:18:20 +0530

    modified the infrastructure

--- tests/unit/test_aws_ecom_century_tmp_stack.py
+++ tests/unit/test_aws_ecom_century_tmp_stack.py
@@ -3,7 +3,7 @@ import aws_cdk.assertions as assertions
 
 from aws_ecom_century_tmp.aws_ecom_century_tmp_stack import AwsEcomCenturyTmpStack
 
-# example tests. To run these tests, uncomment this file along with the example
+# example tests. To run these tests, uncomment this file along with the example as show below
 # resource in aws_ecom_century_tmp/aws_ecom_century_tmp_stack.py
 def test_sqs_queue_created():
     app = core.App()
