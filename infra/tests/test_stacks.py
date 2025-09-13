97d683f7ede6e25c31f845e230bb62f12b71bde2
Gopala krishna Animisetti <44962156+gopala5564@users.noreply.github.com>
2025-09-14 03:01:40 +0530

    fixed tests

--- infra/tests/test_stacks.py
+++ infra/tests/test_stacks.py
@@ -1,14 +1,6 @@
 import aws_cdk as cdk
 import aws_cdk.assertions as assertions
-from infra.stacks.main_stack import MainStack
 from infra.stacks.etl_stack import EtlStack
-
-def test_main_stack_creates_resources():
-    app = cdk.App()
-    stack = MainStack(app, "TestMainStack")
-    template = assertions.Template.from_stack(stack)
-    
-    # Add assertions here for main stack resources
 
 def test_etl_stack_creates_resources():
     app = cdk.App()
@@ -24,11 +16,34 @@ def test_etl_stack_creates_resources():
         "BillingMode": "PAY_PER_REQUEST"
     })
     
-    # Test Lambda function
-    template.resource_count_is("AWS::Lambda::Function", 1)
+    # Test VPC
+    template.resource_count_is("AWS::EC2::VPC", 1)
+    
+    # Test ECS Cluster
+    template.resource_count_is("AWS::ECS::Cluster", 1)
+    
+    # Test ECS Task Definition
+    template.resource_count_is("AWS::ECS::TaskDefinition", 1)
+    template.has_resource_properties("AWS::ECS::TaskDefinition", {
+        "Memory": "4096",
+        "Cpu": "2048"
+    })
     
     # Test SNS Topic
     template.resource_count_is("AWS::SNS::Topic", 1)
     
     # Test EventBridge Rule
     template.resource_count_is("AWS::Events::Rule", 1)
+    
+    # Test IAM Roles
+    template.has_resource_properties("AWS::IAM::Role", {
+        "AssumeRolePolicyDocument": {
+            "Statement": [{
+                "Action": "sts:AssumeRole",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "ecs-tasks.amazonaws.com"
+                }
+            }]
+        }
+    })
